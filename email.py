4b5c0f8265c5b8c6bc1011acda4b3746f99f5fe8
Kristy Lam <150825999+kristy-lam@users.noreply.github.com>
2024-02-20 23:11:43 +0000

    used the not operator to test if a value is False on line 184

--- email.py
+++ email.py
@@ -181,8 +181,7 @@ class Inbox():
                             email.mark_as_read()
                             break
 
-                        elif email_choice == email_num and email.has_been_read \
-                            is True:
+                        elif email_choice == email_num and not email.has_been_read:
                             print("Incorrect input - this email has been read.")
 
                     break
